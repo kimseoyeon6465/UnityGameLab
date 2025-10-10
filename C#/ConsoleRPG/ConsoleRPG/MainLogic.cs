d0b2c04af92ce4114ad6d2d00ef0687243e56993
npckero <npckero@gmail.com>
2025-10-10 12:03:48 +0900

    251010 수업내용

---
+++ C#/ConsoleRPG/ConsoleRPG/MainLogic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRPG
+{
+    internal class MainLogic
+    {
+        // 참조 형태
+        Player player;
+        Monster monster;
+
+        public void Run()
+        {
+            Console.WriteLine("게임 시작");
+
+            // 참조형태 = 실형태
+            player = new Player();
+            monster = new Monster();
+
+            player.Move();
+            monster.Move();
+
+            List<Character> characters = new List<Character>();
+            characters.Add(monster);
+            characters.Add(player);
+
+        }
+    }
+}
---
+++ C#/ConsoleRPG/ConsoleRPG/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleRPG
+{
+    internal class Program
+    {
+        // 정적 -> 정적 메모리 
+        // 스택, 힙, 코드, 데이터
+        // 메인 -> 운영체제 
+        static void Main(string[] args)
+        {
+            MainLogic mainLogic = new MainLogic();
+            mainLogic.Run();
+        }
+
+    }
+}
